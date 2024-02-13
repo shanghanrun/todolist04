a2d73d3c14145ccf84c7895e08aadc55e4098ca5
shanghanrun <idim7@naver.com>
2024-02-13 18:41:25 +0900

    완료를 누를 때,리스트목록삭제되는 문제 해결해야 된다.

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const debugDoneList = document.querySelector('#done-list')
 const shadow = document.querySelector('.shadow')
 const tabs = document.querySelectorAll('.tab')
 tabs.forEach(tab => tab.addEventListener('click', indicator))
+tabs.forEach(tab => addEventListener('click', render))
 
 function indicator(e){
     shadow.style.left = e.target.offsetLeft +'px';
@@ -17,9 +18,9 @@ function indicator(e){
 
 const addButton = document.querySelector('.add')
 const input = document.querySelector('.input')
-const todoList =[]
-const ongoingList=[]
-const doneList =[]
+let todoList =[]
+let ongoingList=[]
+let doneList =[]
 
 
 
@@ -32,7 +33,8 @@ input.addEventListener('keyup', function(e){
 })
 
 function addToList(){
-    const inputValue = input.value.trim();    
+    const inputValue = input.value.trim();
+    console.log('inputValue ', inputValue)    
     const item = {value: inputValue, status: 'ongoing' }
     // 중복값 못들어가게 
     const i = todoList.findIndex(todo => todo.value == inputValue);
@@ -44,7 +46,7 @@ function addToList(){
             console.log(todoList)
             input.value =''
             renderTodoList()
-            showDebug()
+            
         }
     }
     //입력되는 순간 indicator가 '모두'탭을 가리키게 한다.
@@ -69,37 +71,125 @@ function renderTodoList(){
 
     todoList.forEach( item => {
         const li = document.createElement('li')
+        const span = document.createElement('span') 
         li.classList.add('todo')
         li.classList.add(item.status)
         li.setAttribute('data-key', item.value)
          //나중에 getAttribute('data-key')로 받는다.
-        li.innerText = item.value
+        span.innerText = item.value
 
-        const div = document.createElement('div')    
+        const div = document.createElement('div')   
         const doneButton = document.createElement('button')
-        doneButton.innerText = '완료'
-        doneButton.addEventListener('click', doneTodo)
+        doneButton.innerHTML = '완료'
+        doneButton.addEventListener('click', checkTodo)
             
         const deleteButton = document.createElement('button')
-        deleteButton.innerText='삭제'
+        deleteButton.innerHTML='삭제'
         deleteButton.addEventListener('click', deleteTodo)
 
         div.appendChild(doneButton)
         div.appendChild(deleteButton)
+
+        li.appendChild(span)
         li.appendChild(div)
         li.style.display = 'flex';
         li.style.justifyContent = 'space-between'
-        li.style.background ='#b9d4c3'
-        li.style.borderBottom = '1px solid gray'
+        li.style.borderBottom = '1px solid gray';
+        if (item.status == 'ongoing'){
+            li.style.background ='#b9d4c3'
+            li.style.color = 'black'
+        } else{
+            li.style.background ='lightgray'
+            li.style.color = 'gray'
+        }        
     
         todoUl.appendChild(li)
     })
+
+    showDebug()
 }
 
-function doneTodo(){
+function checkTodo(event){
+    const button = event.target;
+    const div = button.parentNode;
+    const li = div.parentNode;
+    const span = li.firstChild
+    const value = span.textContent;
+    const key = li.getAttribute('data-key')
+    console.log('value: ', value)
+    console.log('key :', key)
+    const item = todoList.find( item => item.value == value)
+    console.log(item)
+
+    item.status ='done'
+    console.log(item)
+    // ongoingList에서 해당 item을 삭제한다.
+    ongoingList = ongoingList.filter(item => item.value != value)
+    console.log('ongoingList :', ongoingList)
+    // doneList에 해당 item을 추가한다.
+    doneList = [...doneList, {value: value, status: 'done'}]
+    console.log('doneList :', doneList)
+        
+    renderTodoList();
+    showDebug()
+}
+function deleteTodo(event){
+    const button = event.target;
+    const div = button.parentNode;
+    const li = div.parentNode;
+    const span = li.firstChild
+    const value = span.textContent;
+    const key = li.getAttribute('data-key')
+    
+    ongoingList = ongoingList.filter(item => item.value != value)
+    doneList = doneList.filter(item => item.value != value)
+    todoList = todoList.filter(item => item.value != value)
+
+    renderTodoList();
+    showDebug()
 
 }
-function deleteTodo(){
 
+function render(event){
+    const tab = event.target
+    if (tab.id =='all'){
+        renderTodoList()
+    } else{
+        renderOtherList(tab.id)
+    }
 }
 
+
+function renderOtherList(id){
+    let list =[]
+    if (id == 'ongoing'){
+        list = ongoingList
+    }
+    if (id =='done'){
+        list = doneList
+    }
+
+    todoUl.innerHTML ='';
+
+    list.forEach( item => {
+        const li = document.createElement('li')
+        const span = document.createElement('span') 
+        span.innerText = item.value
+
+        li.appendChild(span)
+        if (item.status == 'ongoing'){
+            li.style.background ='#b9d4c3'
+            li.style.color = 'black'
+        } else{
+            li.style.background ='lightgray'
+            li.style.color = 'gray'
+        }        
+    
+        todoUl.appendChild(li)
+    })
+
+    showDebug()
+}
+
+
+/// 완료를 누를 때, 리스트 목록이 삭제되는 문제 해겷해야 된다.
