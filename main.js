d1fa6253e22db9203d0a75f804ac1f6e5bf5f548
shanghanrun <idim7@naver.com>
2024-02-14 00:59:50 +0900

    .

--- main.js
+++ main.js
@@ -7,12 +7,29 @@ const tabs = document.querySelectorAll('.tab')
 tabs.forEach(tab => tab.addEventListener('click', indicator))
 tabs.forEach(tab => addEventListener('click', render))
 
+const todoClass = document.querySelector('.todos')
 function indicator(e){
+    shadow.style.borderTopRightRadius = '10px';
+
     shadow.style.left = e.target.offsetLeft +'px';
     shadow.style.top = e.target.offsetTop + 'px';
     shadow.style.width = e.target.offsetWidth + 'px';
 
     shadow.style.height = e.target.offsetHeight + 'px'; 
+
+    
+    if(e.target.id == 'all'){
+        shadow.style.background ='rgb(77, 89, 251, 0.8)'
+        todoClass.style.background ='rgb(77, 89, 251, 0.8)';
+    } else if(e.target.id == 'ongoing'){
+        shadow.style.background ='rgba(77, 251, 97, 0.8)'
+        todoClass.style.background ='rgba(77, 251, 97, 0.8)';
+    } else if(e.target.id =='done'){
+        shadow.style.background ='rgba(139, 189, 199, 0.8)'
+        todoClass.style.background ='rgba(139, 189, 199, 0.8)';
+    }
+    todoClass.style.borderTop = '0.1px dotted lightgray';
+    
 }
 
 const addButton = document.querySelector('.add')
@@ -79,15 +96,19 @@ function renderTodoList(){
          //나중에 getAttribute('data-key')로 받는다.
          // 혹은 li.key?
         span.innerText = item.value
+        span.style.padding = '10px';
 
         const div = document.createElement('div')   
         const doneButton = document.createElement('button')
         doneButton.innerHTML = '완료'
         doneButton.addEventListener('click', checkTodo)
+        doneButton.style.padding ='10px';
             
         const deleteButton = document.createElement('button')
         deleteButton.innerHTML='삭제'
         deleteButton.addEventListener('click', deleteTodo)
+        deleteButton.style.padding = '10px';
+        deleteButton.style.marginLeft ='10px';
 
         div.appendChild(doneButton)
         div.appendChild(deleteButton)
@@ -97,8 +118,10 @@ function renderTodoList(){
         li.style.display = 'flex';
         li.style.justifyContent = 'space-between'
         li.style.borderBottom = '1px solid gray';
+        li.style.margin = '10px';
+        li.style.padding = '10px';
         if (item.status == 'ongoing'){
-            li.style.background ='#b9d4c3'
+            li.style.background ='#19f76a'
             li.style.color = 'black'
         } else{
             li.style.background ='lightgray'
@@ -131,16 +154,21 @@ function checkTodo(event){
     // ongoingList에서 해당 item을 삭제한다.
     ongoingList = ongoingList.filter(item => item.value != value)
     console.log('ongoingList :', ongoingList)
+
     // doneList에 해당 item을 추가한다.
-    doneList = [...doneList, {value: value, status: 'done'}]
-    console.log('doneList :', doneList)
-        
+    // 그런데, 이미 추가된 것은 다시 추가되지 않도록 한다.
+    const i = doneList.findIndex( item => item.value ==value)
+    if (i == -1){
+        // 해당 아이템에 없을 경우에 추가한다.
+        doneList = [...doneList, {value: value, status: 'done'}]
+        console.log('doneList :', doneList)       
+    }
 
     // renderTodoList();
     // 바로 다시 랜더하면 화면이 지워진다.
     //그리고 사실 다시 랜더할 필요없이, 아래 사항만 바뀌면 된다.
     if (item.status == 'ongoing'){
-            li.style.background ='#b9d4c3'
+            li.style.background ='#19f76a'
             li.style.color = 'black'
         } else{
             li.style.background ='lightgray'
@@ -196,15 +224,20 @@ function renderOtherList(type){
         const li = document.createElement('li')
         const span = document.createElement('span') 
         span.innerText = item.value
+        span.style.padding = '10px';
 
         li.appendChild(span)
-        if (item.status == 'ongoing'){
-            li.style.background ='#b9d4c3'
-            li.style.color = 'black'
-        } else{
-            li.style.background ='lightgray'
-            li.style.color = 'gray'
-        }        
+        li.style.margin = '10px';
+        li.style.padding = '10px';
+        li.style.borderBottom = '1px solid gray'
+
+        // if (item.status == 'ongoing'){
+        //     li.style.background ='#b9d4c3'
+        //     li.style.color = 'black'
+        // } else{
+        //     li.style.background ='lightgray'
+        //     li.style.color = 'gray'
+        // }        
     
         todoUl.appendChild(li)
     })
